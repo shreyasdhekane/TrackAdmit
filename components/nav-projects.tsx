f662202aa5652226a7bddd1b9757e9ca59b5b7ac
Shreyas Dhekane <120110222+shreyasdhekane@users.noreply.github.com>
2025-08-16 19:09:44 -0400

    Logo update

--- components/nav-projects.tsx
+++ components/nav-projects.tsx
@@ -7,7 +7,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { ArrowUpRight, Link2 } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
 export function NavProjects({
